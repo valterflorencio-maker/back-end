399f4321bcdfaf5be2478298c15a9908df877bbd
valterflorencio-maker <valter.florencio@escola.pr.gov.br>
2026-03-09 08:45:54 -0300

    Create cont03.py

---
+++ conteudo/cont03.py
@@ -0,0 +1,10 @@
+n = input('digite algo')
+print(n.isnumeric())   # para perguntar se é um número
+print(n.isalpha())     # para perguntar se é letra
+print(n.isalnum())     # para perguntar se é letra ou número
+print(n.isupper())     # para perguntar se é só com letras maiúsculas
+print(n.isdecimal())   # para verificar se é decimal
+print(n.isdigit())     # para verificar se é um dígito
+print(n.islower())     # para verificar se é apenas letras minúsculas
+print(n.isidentifier())# para verificar se é um identificador válido
+print(n.isspace())     # para verificar se tem espaço em branco
