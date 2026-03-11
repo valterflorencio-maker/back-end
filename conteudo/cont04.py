7a152d0a042dd8a0995fd4c4d99e450658869718
valterflorencio-maker <valter.florencio@escola.pr.gov.br>
2026-03-11 11:31:00 -0300

    Add comments explaining arithmetic operators in Python

---
+++ conteudo/cont04.py
@@ -0,0 +1,13 @@
+#O + é utilizado como operador de adição
+
+#O - é utilizado como sinal de subtração
+
+#O * é utilizado para fazer a multiplicação
+
+#O / é utilizada para fazer a divisão
+
+#O ** é utilizado para fazer a exponenciação
+
+#O // é utilizado para fazer a divisão inteira
+
+#O % é utilizada para o resto da divisão
