704f46cf393bba17081243a21182965689e1b4f4
valterflorencio-maker <valter.florencio@escola.pr.gov.br>
2026-03-16 10:35:56 -0300

    Create cont05.py

---
+++ conteudo/cont05.py
@@ -0,0 +1,9 @@
+#Definição: é a ordem na qual os operadores serão executados
+
+#1ª Expressões dentro de parenteses
+
+#2ª Em seguida vem as potências
+
+#3ª Em terceiro lugar vem a multiplicação, a divisão normal, a divisão inteira e o resto da divisão. No caso de ter mais de um deles você resolve quem aparecer primeiro
+
+#4ª E por último a soma e a subtração
