456b17d4a4483803e627592985b8b5deefc6314f
valterflorencio-maker <valter.florencio@escola.pr.gov.br>
2026-03-16 10:38:41 -0300

    Update cont05.py

--- conteudo/cont05.py
+++ conteudo/cont05.py
@@ -7,3 +7,11 @@
 #3ª Em terceiro lugar vem a multiplicação, a divisão normal, a divisão inteira e o resto da divisão. No caso de ter mais de um deles você resolve quem aparecer primeiro
 
 #4ª E por último a soma e a subtração
+
+#5+3*2==
+
+#3*5+4**2=
+
+#3*(5+4)**2=
+
+#O Python é utilizado em cálculos científicos é o tamanho da memoria ou seja conseguimos chegar a números extremamente grandes
