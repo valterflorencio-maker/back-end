c744b943272c28b5b8d239c629d12c3c6612337b
valterflorencio-maker <valter.florencio@escola.pr.gov.br>
2026-03-19 08:11:20 -0300

    Create ex008.py

---
+++ Exercicios/ex008.py
@@ -0,0 +1,6 @@
+n1 = float(input('digite a primeira nota: '))
+n2 = float(input('digite a segunda nota: '))
+if n1 > 10 or n2 > 10:
+    print('erro numero invalido')
+else:
+    print('A 1\u00ba nota: {} 2\u00ba nota {} e a Media {}'.format(n1, n2, n1 + n2 / 2))
