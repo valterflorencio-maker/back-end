bc969bb404004bb18ad74cd48623b727865ad453
valterflorencio-maker <valter.florencio@escola.pr.gov.br>
2026-03-26 08:21:01 -0300

    Document math library usage in cont08.py
    Add documentation for the math library in cont08.py

---
+++ conteudo/cont08.py
@@ -0,0 +1,4 @@
+#BIBLIOTECA MATH
+#Essa biblioteca e bem utilizada no Python e seu nome significa matemática.
+
+#Quando importada ela traz diversos recursos matemáticos como por exemplo a funcionalidade ceil que arredonda números pra cima ou a funcionalidade floor que realiza o arredondamento pra baixo.
