cd8889df43bbb7b2595d14facdc549fe7f3effc9
valterflorencio-maker <valter.florencio@escola.pr.gov.br>
2026-03-26 08:45:28 -0300

    Refactor math operations in cont09.py
    Refactor code to import specific math functions and simplify calculations.

--- conteudo/cont09.py
+++ conteudo/cont09.py
@@ -3,3 +3,10 @@ num = int(input('Digite um número: ' ))
 raiz = math.sqrt(num)
 print('a raiz de {} é igual a {}'.format(num, math.ceil(raiz)))
 print('a raiz de {} é igual a {}'.format(num, math.floor(raiz)))
+
+#importando funcionalidades específicas
+from math import sqrt, floor, ceil
+num = int(input('Digite um número: ' ))
+raiz = sqrt(num)
+print('a raiz de {} é igual a {}'.format(num, ceil(raiz)))
+print('a raiz de {} é igual a {}'.format(num, floor(raiz)))
