e5cdd7bf790e1b99d99a047d9ff80dc26e148cf0
valterflorencio-maker <valter.florencio@escola.pr.gov.br>
2026-03-26 08:19:02 -0300

    Create cont07.py

---
+++ conteudo/cont07.py
@@ -0,0 +1,4 @@
+#MÓDULOS EM PYTHON
+#Para importar bibliotecas e funcionalidades em Python utiliza-se o comando Import seguido do nome da biblioteca ou módulo que ira ser carregado nesse programa.
+
+#Para importar coisa específicas de uma biblioteca utiliza-se o comando from seguido do nome da biblioteca seguido da palavra import seguido do item que deseja importar.
