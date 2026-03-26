ee4d127b19288d9e8d7ca1335b1acb3ad9be5d4e
valterflorencio-maker <valter.florencio@escola.pr.gov.br>
2026-03-26 08:09:52 -0300

    Create ex016.py

---
+++ Exercicios/ex016.py
@@ -0,0 +1,3 @@
+km = int(input('Digite a quantidade de KM rodados: '))
+dias = int(input('Digite por quantos dias obeteve o carro: '))
+print('Voce rodou {}km, deve pagar {}R$, Alugou o carro por {}, deve pagar {}R$'.format(km, km * 0.15, dias, dias * 60))
