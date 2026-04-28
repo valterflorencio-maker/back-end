536acdf33ddc02aa683544682735d09e8fcb37af
valterflorencio-maker <valter.florencio@escola.pr.gov.br>
2026-04-28 12:21:51 -0300

    Create ex020.py

---
+++ Exercicios/ex020.py
@@ -0,0 +1,25 @@
+from random-import choice   
+
+
+aluno1 = str(input('Primeiro aluno: '))   
+
+
+aluno2   
+
+
+str(input('Segundo aluno:'))   
+
+
+aluno3 = str(input('Terceiro aluno:  ))   
+
+
+aluno4 = str(input('Quarto aluno:'))   
+
+
+lista [alunol, aluno2, aluno3, aluno4]   
+
+
+escolhido choice(lista) #escolhe um da lista   
+
+
+print('o aluno escolhido foi ()'.format(escolhido))
